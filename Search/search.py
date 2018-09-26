422c5aa263aeae6e3d961e4224d9f5c4021182d4
infsolution <clsinfsolution@gmail.com>
2018-09-26 15:49:50 -0300

    Update

--- Search/search.py
+++ Search/search.py
@@ -4,33 +4,46 @@ import re
 
 class Engine:
 
-	def __init__(self, url, wordSearch, depth):
-		self.url = url
+	def __init__(self, wordSearch):
 		self.wordSearch = wordSearch
-		self.depth = depth
 		self.blackLink = []
 		
-	def searchPage(self):
-		if self.url not in self.blackLink:
+	def searchPage(self, url):
+		if url not in self.blackLink:
 			links = []
-			r = requests.get(self.url) # requests.get pega a response da url
+			try:
+				r = requests.get(url) # requests.get pega a response da url
+			except ConnectionRefusedError as e:
+				print(e)
+				r = "No response"	
+			except ConnectionError as e:
+				print(e)
+				r = "No response"
 			html_page = BeautifulSoup(r.text,'html.parser')# Analiza o html para a extração de dados
 			pattern = re.compile(r'href="(.*?)"')#cria um pattern que pega todos os links da page
 			for i in r.text.split(): #faz um for em todas as "linhas" do documento html
 				if 'http' in i or 'https' in i:	#verifica se a linha contem http ou https
 					links += pattern.findall(i) #adiciona o link à lista
 			numLink = len(links)		
-			print("Foram encontrados "+str(len(links))+" Links em "+self.url+":")
+			print("Foram encontrados "+str(len(links))+" Links em "+url+":")
 			#print(links)
-			self.blackLink.append(self.url)
+			self.blackLink.append(url)
 			word = html_page.text.find(self.wordSearch)
-			wordS = html_page.text[word:word+len(self.wordSearch)]
+			wordS = html_page.text[word-10:word+len(self.wordSearch)+10]
 			if len(wordS)>0 :
 				print("A busca encontrou a palavra: "+wordS)
 			else:
 				print("Nada foi encontrado!")
 			return links
 
+	def engineSearch(self, urls,depth):
+				lk = []
+				if depth > 0:
+					for i in urls:
+						lk = self.searchPage(i)
+					self.engineSearch(lk, depth-1)	
+				else:
+					print("END")
 
 
 
@@ -39,5 +52,5 @@ class Engine:
 
 
 
-engine = Engine("http://www.globo.com", "entretenimento", 1)
-engine.searchPage()
+engine = Engine( "Notícia")
+engine.engineSearch(["https://grupoglobo.globo.com"],1)
