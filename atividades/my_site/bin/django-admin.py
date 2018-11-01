f329d670185e3e2f391b3339009063dde2c933b7
infsolution <clsinfsolution@gmail.com>
2018-10-31 21:34:46 -0300

    create project of activity mysite

---
+++ atividades/my_site/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/cicero/Documentos/IFPI/4PERIODO/PROGINTERNET/GIT/ProgInternet2018/atividades/my_site/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
