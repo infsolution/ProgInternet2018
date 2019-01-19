36f61c0cf6fe425d0e71280564f380ee17324420
infsolution <clsinfsolution@gmail.com>
2019-01-19 10:24:40 -0300

    conclusão atividade gameapi

--- gamesapi/games/serializers.py
+++ gamesapi/games/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-from .models import Game
+from .models import *
+class GameCategorySerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = GameCategory
+		fields = ('url', 'pk', 'name', 'games')
+
 class GameSerializer(serializers.ModelSerializer):
 	#name = serializers.CharField(max_length=200, validators=[UniqueValidator(queryset=Game.objects.all(), message="This name already")])
 	game_category = serializers.SlugRelatedField(queryset=GameCategory.objects.all(),slug_field='name')
@@ -9,6 +14,14 @@ class GameSerializer(serializers.ModelSerializer):
 		fields = ('url', 'game_category', 'name', 'release_date', 'played')
 
 class ScoreSerializer(serializers.HyperlinkedModelSerializer):
+	game = serializers.SlugRelatedField(queryset=Game.objects.all(), slug_field='name')
+	player = serializers.SlugRelatedField(queryset=Player.objects.all(), slug_field='name')
 	class Meta:
-	model = Score
-	fields = ('url',	' pk', 'score', 'score_date', 'player', 'game')
+		model = Score
+		fields = ('url', 'pk', 'score', 'score_date', 'player', 'game')
+
+class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+	scores = ScoreSerializer(many=True, read_only=True)
+	class Meta:
+		model = Player
+		fields = ('url', 'name', 'gender', 'scores')
--- gamesapi/games/views.py
+++ gamesapi/games/views.py
@@ -5,15 +5,61 @@ Author: Gaston C. Hillar - Twitter.com/gastonhillar
 Publisher: Packt Publishing Ltd. - http://www.packtpub.com
 """
 from rest_framework.parsers import JSONParser
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from games.models import Game
-from games.serializers import GameSerializer
+from games.models import *
+from games.serializers import *
 from datetime import datetime
 from django.utils import timezone
 
-@api_view(['GET', 'POST'])
+class GameCategoryList(generics.ListCreateAPIView):
+	queryset = GameCategory.objects.all()
+	serializer_class = GameCategorySerializer
+	name='gamecategory-list'
+class GameCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = GameCategory.objects.all()
+	serializer_class = GameCategorySerializer
+	name = 'gamecategory-detail'
+
+class GameList(generics.ListCreateAPIView):
+	queryset = Game.objects.all()
+	serializer_class = GameSerializer
+	name = 'game-list'
+class GameDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Game.objects.all()
+	serializer_class = GameSerializer
+	name = 'game-detail'
+
+class PlayerList(generics.ListCreateAPIView):
+	queryset = Player.objects.all()
+	serializer_class = PlayerSerializer
+	name = 'player-list'
+class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Player.objects.all()
+	serializer_class = PlayerSerializer
+	name = 'player-detail'
+class ScoreList(generics.ListCreateAPIView):
+	queryset = Score.objects.all()
+	serializer_class = ScoreSerializer
+	name = 'score-list'
+class ScoreDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Score.objects.all()
+	serializer_class = ScoreSerializer
+	name = 'score-detail'
+
+
+class ApiRoot(generics.GenericAPIView):
+	name = 'api-root'
+	def get(self, request, *args, **kwargs):
+		return Response({ 'players': reverse(PlayerList.name, request=request),
+			'game-categories': reverse(GameCategoryList.name, request=request),
+			'games': reverse(GameList.name, request=request),
+			'scores': reverse(ScoreList.name, request=request)})
+
+
+
+'''@api_view(['GET', 'POST'])
 def game_list(request):
 	if request.method == 'GET':
 		games = Game.objects.all()
@@ -46,4 +92,4 @@ def game_detail(request, game_id):
 		if game.release_date > now:
 			game.delete()
 			return Response(status=status.HTTP_202_ACCEPTED)#status.HTTP_204_NO_CONTENT
-		return Response(status=status.HTTP_412_PRECONDITION_FAILED)
+		return Response(status=status.HTTP_412_PRECONDITION_FAILED)'''
