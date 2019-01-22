5134a9b6dd0aa9eb97cd1fde665bf7bec20e28ce
Infoslution <clsinfsolution@gmail.com>
2019-01-22 16:56:43 -0300

    auth config

--- gamesapi/gamesapi/settings.py
+++ gamesapi/gamesapi/settings.py
@@ -107,6 +107,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
     'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 5
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework.authentication.BasicAuthentication',
+    'rest_framework.authentication.SessionAuthentication',
+    )
 }
 
 # Internationalization
